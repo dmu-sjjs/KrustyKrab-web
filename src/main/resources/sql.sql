30c92d546d126c2f8dc3d8ebe811752e0fe914bc
JUN <chltmdwns96@naver.com>
2022-12-01 14:20:20 +0900

    admin 계정 생성 sql문 수정

--- src/main/resources/sql.sql
+++ src/main/resources/sql.sql
@@ -3,7 +3,7 @@ create database krustykrabdb;
 use krustykrabdb;
 
 -- admin계정 생성
-insert into user Values(0,'ADMIN','admin','admin','admin','admin','admin','admin','admin');
+insert into user Values(0,'ADMIN','ADMIN','ADMIN','ADMIN','ADMIN','ADMIN','ADMIN');
 
 -- menu 데이터 삭제
 set sql_safe_updates=0;
