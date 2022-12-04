5797597e47e0f757b8b6dcb2d72587ea72e043aa
JUN <chltmdwns96@naver.com>
2022-12-04 19:54:42 +0900

    장바구니 domain 구현

---
+++ src/main/java/js/krustykrab/domain/cart/Cart.java
@@ -0,0 +1,16 @@
+package js.krustykrab.domain.cart;
+
+
+import java.util.List;
+
+public class Cart {
+    private final List<CartItem> cart;
+
+    public Cart(List<CartItem> cart) {
+        this.cart = cart;
+    }
+
+    public List<CartItem> getCart() {
+        return cart;
+    }
+}
