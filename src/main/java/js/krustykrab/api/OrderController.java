fa0227e925b7cb6b15a072c40c66236661dfc4bb
JUN <chltmdwns96@naver.com>
2022-12-04 18:33:12 +0900

    toOrderPage API를 PathVariable 방식으로 변경

--- src/main/java/js/krustykrab/api/OrderController.java
+++ src/main/java/js/krustykrab/api/OrderController.java
@@ -2,7 +2,9 @@ package js.krustykrab.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/order")
 public class OrderController {
 
-    @GetMapping
-    public String toOrderPage() {
+    @GetMapping("/{storeId}")
+    public String toOrderPage(@PathVariable Long storeId, Model model) {
+        model.addAttribute("storeId", storeId);
         return "/order/orderPage";
     }
 
