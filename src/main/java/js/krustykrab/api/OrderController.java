edc6e0297b7bd623866d056e187b085cc399eacd
JUN <chltmdwns96@naver.com>
2022-12-04 18:21:27 +0900

    order 컨트롤러 api 매핑

--- src/main/java/js/krustykrab/api/OrderController.java
+++ src/main/java/js/krustykrab/api/OrderController.java
@@ -2,6 +2,7 @@ package js.krustykrab.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/order")
 public class OrderController {
 
+    @GetMapping
     public String toOrderPage() {
         return "/order/orderPage";
     }
 
+    @GetMapping("/select")
     public String toMenuSelectPage() {
         return "/order/menuSelectPage";
     }
