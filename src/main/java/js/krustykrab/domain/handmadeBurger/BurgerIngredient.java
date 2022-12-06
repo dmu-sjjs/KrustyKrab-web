974407e386b24bb8dd2e09a14f4339ea11aa4389
JUN <chltmdwns96@naver.com>
2022-12-07 00:28:24 +0900

    수제버거 domain 구현

---
+++ src/main/java/js/krustykrab/domain/handmadeBurger/BurgerIngredient.java
@@ -0,0 +1,8 @@
+package js.krustykrab.domain.handmadeBurger;
+
+public enum BurgerIngredient {
+    LETTUCE, ONION, TOMATO, // Vegetable
+    COW, CHICKEN, SHRIMP,   // Patty
+    PICKLE, GARLIC, EGGFRY, // Topping
+    BBQ, CHILI, RANCH   // Source
+}
---
+++ src/main/java/js/krustykrab/domain/handmadeBurger/HandmadeBurgerOrderDetail.java
@@ -0,0 +1,73 @@
+package js.krustykrab.domain.handmadeBurger;
+
+import js.krustykrab.domain.menu.Menu;
+import js.krustykrab.domain.order.Order;
+import js.krustykrab.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "HANDMADE_BURGER_ORDER_DETAIL")
+public class HandmadeBurgerOrderDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hbOrderInfoId")
+    private Long hbOrderInfoId;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "orderId")
+    private Order order;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "menuId")
+    private Menu menu;
+
+    @Column(name = "vegetable1")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient vegetable1;
+
+    @Column(name = "vegetable2")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient vegetable2;
+
+    @Column(name = "vegetable3")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient vegetable3;
+
+    @Column(name = "patty1")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient patty1;
+
+    @Column(name = "patty2")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient patty2;
+
+    @Column(name = "topping1")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient topping1;
+
+    @Column(name = "topping2")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient topping2;
+
+    @Column(name = "topping3")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient topping3;
+
+    @Column(name = "source1")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient source1;
+
+    @Column(name = "source2")
+    @Enumerated(value = EnumType.STRING)
+    private BurgerIngredient source2;
+}
