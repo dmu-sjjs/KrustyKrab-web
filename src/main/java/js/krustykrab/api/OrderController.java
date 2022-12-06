22ed8bf27385d76b6309211f42ae342e27704bd5
JUN <chltmdwns96@naver.com>
2022-12-06 20:48:04 +0900

    OrderPage 이동 시 model에 All Menu Data 추가

--- src/main/java/js/krustykrab/api/OrderController.java
+++ src/main/java/js/krustykrab/api/OrderController.java
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,6 +39,7 @@ public class OrderController {
         if (userDto != null) {
             session.setAttribute("cart", new Cart());
             model.addAttribute("storeId", storeId);
+            model.addAttribute("menus", menuService.findAllMenu());
             return "/order/orderPage";
         }
 
